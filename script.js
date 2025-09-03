820803a600d2df87f3456e8421c0fcc0addf4fad
Ftass <f.bustamantegajardo@uandresbello.edu>
2025-09-02 21:16:57 -0400

    primer diseño

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const img = document.getElementById("mg-img");
+const btn = document.getElementById("toggler");
+btn.addEventListener("click", () => {
+  img.classList.toggle("toggled-img"); 
+});
+
